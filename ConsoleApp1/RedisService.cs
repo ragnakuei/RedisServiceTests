efa0b752309c7c67643bf9f614d3b087f1d2a9cc
Kuei <shiankuei@data-sci.info>
2022-03-18 20:16:16 +0800

    refactor

--- ConsoleApp1/RedisService.cs
+++ ConsoleApp1/RedisService.cs
@@ -6,55 +6,65 @@ public class RedisService
 {
     static RedisService()
     {
-        lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-                                                         {
-                                                             var conn = ConnectionMultiplexer.Connect("localhost:6379");
-                                                             conn.ConfigurationChanged += (object sender, EndPointEventArgs e) =>
-                                                                                          {
-                                                                                              Console.WriteLine("配置更改");
-                                                                                          };
-                                                             conn.ConfigurationChangedBroadcast += (object sender, EndPointEventArgs e) =>
-                                                                                                   {
-                                                                                                       Console.WriteLine("通過發布訂閱更新配置");
-                                                                                                   };
-                                                             conn.ConnectionFailed += (object sender, ConnectionFailedEventArgs e) =>
-                                                                                      {
-                                                                                          Console.WriteLine("連接失敗");
-                                                                                      };
-                                                             conn.ConnectionRestored += (object sender, ConnectionFailedEventArgs e) =>
-                                                                                        {
-                                                                                            Console.WriteLine("重新建立連接到之前出錯的連接");
-                                                                                        };
-                                                             conn.ErrorMessage += (object sender, RedisErrorEventArgs e) =>
-                                                                                  {
-                                                                                      Console.WriteLine("發生錯誤");
-                                                                                  };
-                                                             conn.HashSlotMoved += (object sender, HashSlotMovedEventArgs e) =>
+        _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+                                                          {
+                                                              var conn = ConnectionMultiplexer.Connect("localhost:6379");
+                                                              conn.ConfigurationChanged += (object sender, EndPointEventArgs e) =>
+                                                                                           {
+                                                                                               Console.WriteLine("配置更改");
+                                                                                           };
+                                                              conn.ConfigurationChangedBroadcast += (object sender, EndPointEventArgs e) =>
+                                                                                                    {
+                                                                                                        Console.WriteLine("通過發布訂閱更新配置");
+                                                                                                    };
+                                                              conn.ConnectionFailed += (object sender, ConnectionFailedEventArgs e) =>
+                                                                                       {
+                                                                                           Console.WriteLine("連接失敗");
+                                                                                       };
+                                                              conn.ConnectionRestored += (object sender, ConnectionFailedEventArgs e) =>
+                                                                                         {
+                                                                                             Console.WriteLine("重新建立連接到之前出錯的連接");
+                                                                                         };
+                                                              conn.ErrorMessage += (object sender, RedisErrorEventArgs e) =>
                                                                                    {
-                                                                                       Console.WriteLine("更改集群");
+                                                                                       Console.WriteLine("發生錯誤");
                                                                                    };
-                                                             conn.InternalError += (object sender, InternalErrorEventArgs e) =>
-                                                                                   {
-                                                                                       Console.WriteLine("redis庫內部錯誤");
-                                                                                   };
-                                                             return conn;
-                                                         });
+                                                              conn.HashSlotMoved += (object sender, HashSlotMovedEventArgs e) =>
+                                                                                    {
+                                                                                        Console.WriteLine("更改集群");
+                                                                                    };
+                                                              conn.InternalError += (object sender, InternalErrorEventArgs e) =>
+                                                                                    {
+                                                                                        Console.WriteLine("redis庫內部錯誤");
+                                                                                    };
+                                                              return conn;
+                                                          });
     }
 
-    private static Lazy<ConnectionMultiplexer> lazyConnection;
-
-    private static ConnectionMultiplexer Connection => lazyConnection.Value;
+    private static Lazy<ConnectionMultiplexer> _lazyConnection;
 
     private IDatabase GetDatabase()
     {
-        return Connection.GetDatabase();
+        return _lazyConnection.Value.GetDatabase();
     }
 
+    /// <summary>
+    /// Key Lock
+    /// </summary>
+    /// <param name="key">key 名稱</param>
+    /// <param name="expiry">Lock 自動 release 的時間長度</param>
+    /// <param name="func">Lock 後要執行的動作</param>
+    /// <param name="maxRetryCount">最大重試次數</param>
+    /// <param name="retryDelayMs">每次重試的延遲等待時間，單位：ms</param>
+    /// <param name="exitWhenExceptionOccurs">Exception 發生時，是否直接跳出</param>
+    /// <returns></returns>
     public async Task<bool> LockTakeAsync(string     key,
                                           TimeSpan   expiry,
                                           Func<Task> func,
-                                          int        maxRetryCount = 20,
-                                          int        retryDelayMs  = 1000)
+                                          int        maxRetryCount           = 20,
+                                          int        retryDelayMs            = 1000,
+                                          bool       exitWhenExceptionOccurs = true)
+
     {
         // Lock失敗就等 1000 毫秒，再重試，最多 20 次
         var lockKey           = $"Lock_{key}";
@@ -74,13 +84,17 @@ public class RedisService
                 }
                 else
                 {
-                    // await Task.Delay(retryDelayMs);
                     Console.WriteLine($"LockTakeAsync False, retry count: {currentRetryCount}");
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Redis Service Exception Occurs: {e.GetType().Name} {e.Message}");
+
+                if (exitWhenExceptionOccurs)
+                {
+                    return false;
+                }
             }
 
             await Task.Delay(retryDelayMs);
